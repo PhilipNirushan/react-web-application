be70de512faf294d26432632871b4db2c932f2c6
Philip Nirushan <philipnirushan25@gmail.com>
2023-03-19 21:22:50 +0530

    NavBar Created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import GlobalStyle from "./globalStyles";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+//Components
+import Navbar from "./components/Navbar/Navbar";
+
 //Pages
 import Home from "./pages/Home";
 
@@ -10,6 +13,7 @@ function App() {
     <div className="App">
       <Router>
         <GlobalStyle />
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
